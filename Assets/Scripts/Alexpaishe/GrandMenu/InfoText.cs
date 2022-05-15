f484a5df6adced4da81b33113dcc1416b33a9544
AlexPaishe <63985032+AlexPaishe@users.noreply.github.com>
2022-05-15 12:08:52 +0300

    IconMenu

--- Assets/Scripts/Alexpaishe/GrandMenu/InfoText.cs
+++ Assets/Scripts/Alexpaishe/GrandMenu/InfoText.cs
@@ -1,10 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class InfoText : MonoBehaviour
 {
-    [SerializeField] private string[] _textWeapon;
-    [SerializeField] private string[] _textEnemy;
+    [SerializeField] private string[] _textInfo;
+    [SerializeField] private Text _textInformationTitle;
+    [SerializeField] private Image _icon;
+    [SerializeField] private Sprite[] _spriteIcon;
 
+    /// <summary>
+    /// Реализация изменения текста и картинки под нужную вариацию
+    /// </summary>
+    /// <param name="variation"></param>
+    public void VariationInformation(int variation)
+    {
+        for(int i = 0; i < _textInfo.Length; i++)
+        {
+            _icon.sprite = _spriteIcon[variation];
+            _textInformationTitle.text = _textInfo[variation];
+        }
+    }
 }
