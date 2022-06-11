a815109b18d0885bd2d426820769232f04c11786
VakulaGames <vakulinivan50@gmail.com>
2022-06-11 22:01:39 +0300

    fix blaster

--- Assets/Scripts/Ivan/Player/PlayerRotation.cs
+++ Assets/Scripts/Ivan/Player/PlayerRotation.cs
@@ -7,6 +7,8 @@ public class PlayerRotation : MonoBehaviour
     [SerializeField] private ShootJoystick _shootJoystick;
     [SerializeField] private Transform _playerTorso;
     [SerializeField] private Transform _playerLegs;
+
+    public bool BlockedRotate { get; set; }
  
     private Rigidbody _rigidbody;
 
@@ -17,15 +19,18 @@ public class PlayerRotation : MonoBehaviour
 
     void Update()
     {
-        if (_shootJoystick.Direction != Vector2.zero)
+        if(BlockedRotate == false)
         {
-            Vector3 direction = new Vector3(_shootJoystick.Horizontal, 0, _shootJoystick.Vertical);
-            _playerTorso.rotation = Quaternion.LookRotation(direction);
-        }
-        else
-        {
-            Vector3 direction = new Vector3(_moveJoystick.Horizontal, 0, _moveJoystick.Vertical);
-            _playerTorso.rotation = _playerLegs.rotation;
+            if (_shootJoystick.Direction != Vector2.zero)
+            {
+                Vector3 direction = new Vector3(_shootJoystick.Horizontal, 0, _shootJoystick.Vertical);
+                _playerTorso.rotation = Quaternion.LookRotation(direction);
+            }
+            else
+            {
+                Vector3 direction = new Vector3(_moveJoystick.Horizontal, 0, _moveJoystick.Vertical);
+                _playerTorso.rotation = _playerLegs.rotation;
+            }
         }
 
         if (_moveJoystick.Direction != Vector2.zero)
